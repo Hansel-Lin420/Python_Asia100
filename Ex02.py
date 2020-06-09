88ff57dd97e23a2a600ac5f785956423334c6db4
asdasd950 <o60417o@yahoo.com.tw>
2020-06-10 01:38:07 +0800

    2.0

--- Ex02.py
+++ Ex02.py
@@ -8,5 +8,5 @@ Created on Sun May 17 16:17:50 2020
 input_str=input();
 reverse_str = ''
 for i in input_str:
-    reverse_str = i + reverse_str 
+    reverse_str = i + reverse_str
 print(reverse_str)
